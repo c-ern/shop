d616da4a6a52f24630b39ff4e6cea5cc8ffe855f
Steve Lair <steve.lair@gmx.de>
2012-08-21 15:55:17 +0200

    Nackt, nach rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = '40aeb801762903cf63e95b75c51f3a04ca2e960a77993aa04adcbf5e90ed956ddbe732a8c45089fcc86fb636509568fa25209ad6373648804046645f262e59f7'
